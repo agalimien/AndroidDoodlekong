373202521bb93dddae93aff85d8d27987daf1be9
Harvey Tai <40065121+agalimien@users.noreply.github.com>
2021-07-04 21:40:23 +0800

    Part44 ScarletSetup ok

---
+++ app/src/main/java/com/plcoding/doodlekong/data/remote/ws/models/DrawAction.kt
@@ -0,0 +1,12 @@
+package com.plcoding.doodlekong.data.remote.ws.models
+
+import com.plcoding.doodlekong.util.Constants.TYPE_DRAW_ACTION
+
+data class DrawAction(
+    val action: String
+) : BaseModel(TYPE_DRAW_ACTION) {
+
+    companion object {
+        const val ACTION_UNDO = "ACTION_UNDO"
+    }
+}
--- app/src/main/java/com/plcoding/doodlekong/ui/drawing/DrawingViewModel.kt
+++ app/src/main/java/com/plcoding/doodlekong/ui/drawing/DrawingViewModel.kt
@@ -3,6 +3,7 @@ package com.plcoding.doodlekong.ui.drawing
 import androidx.lifecycle.ViewModel
 import com.google.gson.Gson
 import com.plcoding.doodlekong.R
+import com.plcoding.doodlekong.data.remote.ws.DrawingApi
 import com.plcoding.doodlekong.util.DispatcherProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -11,9 +12,10 @@ import javax.inject.Inject
 
 @HiltViewModel
 class DrawingViewModel @Inject constructor(
+    private val drawingApi: DrawingApi,
     private val dispatchers: DispatcherProvider,
     private val gson: Gson
-): ViewModel() {
+) : ViewModel() {
 
     private val _selectedColorButtonId = MutableStateFlow(R.id.rbBlack)
     val selectedColorButtonId: StateFlow<Int> = _selectedColorButtonId
