ff81272300f187d4ff8a82e45e4dfcd38268d6f1
Harvey Tai <40065121+agalimien@users.noreply.github.com>
2021-07-06 11:45:03 +0800

    Part49 DrawingViewSetupReceiveDrawData ok

--- app/src/main/java/com/plcoding/doodlekong/ui/views/DrawingView.kt
+++ app/src/main/java/com/plcoding/doodlekong/ui/views/DrawingView.kt
@@ -39,6 +39,7 @@ class DrawingView @JvmOverloads constructor(
     private var paths = Stack<PathData>()
     private var pathDataChangedListener: ((Stack<PathData>) -> Unit)? = null
 
+    private var startedTouch = false
     var roomName: String? = null
     var isUserDrawing = false
         set(value) {
@@ -80,6 +81,63 @@ class DrawingView @JvmOverloads constructor(
             strokeWidth = initialThickness
         }
         canvas?.drawPath(path, paint)
+    }
+
+    fun startedTouchExternally(drawData: DrawData) {
+        parseDrawData(drawData).apply {
+            paint.color = color
+            paint.strokeWidth = thickness
+
+            path.reset()
+            path.moveTo(fromX, fromY)
+            invalidate()
+
+            startedTouch = true
+        }
+    }
+
+    fun movedTouchExternally(drawData: DrawData) {
+        parseDrawData(drawData).apply {
+            val dx = abs(toX - fromX)
+            val dy = abs(toY - fromY)
+            if (!startedTouch) {
+                startedTouchExternally(drawData)
+            }
+            if (dx >= smoothness || dy >= smoothness) {
+                path.quadTo(fromX, fromY, (fromX + toX) / 2f, (fromY + toY) / 2f)
+                invalidate()
+            }
+        }
+    }
+
+    fun releasedTouchExternally(drawData: DrawData) {
+        parseDrawData(drawData).apply {
+            path.lineTo(fromX, fromY)
+            canvas?.drawPath(path, paint)
+            paths.push(PathData(path, paint.color, paint.strokeWidth))
+            pathDataChangedListener?.let { pathsStackFn ->
+                pathsStackFn(paths)
+            }
+            path = Path()
+            invalidate()
+            startedTouch = false
+        }
+    }
+
+    override fun setEnabled(enabled: Boolean) {
+        super.setEnabled(enabled)
+        path.reset()
+        invalidate()
+    }
+
+    fun undo() {
+        if (paths.isNotEmpty()) {
+            paths.pop()
+            pathDataChangedListener?.let { pathsStackFn ->
+                pathsStackFn(paths)
+            }
+            invalidate()
+        }
     }
 
     private fun startedTouch(x: Float, y: Float) {
@@ -141,6 +199,15 @@ class DrawingView @JvmOverloads constructor(
             ACTION_UP -> releasedTouch()
         }
         return true
+    }
+
+    private fun parseDrawData(drawData: DrawData): DrawData {
+        return drawData.copy(
+            fromX = drawData.fromX * viewWidth!!,
+            fromY = drawData.fromY * viewHeight!!,
+            toX = drawData.toX * viewWidth!!,
+            toY = drawData.toY * viewHeight!!
+        )
     }
 
     private fun createDrawData(
