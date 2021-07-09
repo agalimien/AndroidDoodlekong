14e180a6f9e632fe2651fd20ea8c5ddf16d08f76
Harvey Tai <40065121+agalimien@users.noreply.github.com>
2021-07-09 11:24:35 +0800

    Part53 ObservingPhaseChanges ok

--- app/src/main/java/com/plcoding/doodlekong/ui/views/DrawingView.kt
+++ app/src/main/java/com/plcoding/doodlekong/ui/views/DrawingView.kt
@@ -229,6 +229,19 @@ class DrawingView @JvmOverloads constructor(
         )
     }
 
+    fun finishOffDrawing() {
+        isDrawing = false
+        path.lineTo(curX ?: return, curY ?: return)
+        canvas?.drawPath(path, paint)
+
+        paths.push(PathData(path, paint.color, paint.strokeWidth))
+        pathDataChangedListener?.let { pathsStackFn ->
+            pathsStackFn(paths)
+        }
+        path = Path()
+        invalidate()
+    }
+
     fun setThickness(thickness: Float) {
         paint.strokeWidth = thickness
     }
