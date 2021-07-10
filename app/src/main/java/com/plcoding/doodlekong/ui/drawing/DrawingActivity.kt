0c86718efd8b5e8c73542bc959cc99981cddb2f7
Harvey Tai <40065121+agalimien@users.noreply.github.com>
2021-07-10 15:32:58 +0800

    Part59 ImplementingTheLeaveDialog ok

--- app/src/main/java/com/plcoding/doodlekong/ui/drawing/DrawingActivity.kt
+++ app/src/main/java/com/plcoding/doodlekong/ui/drawing/DrawingActivity.kt
@@ -23,6 +23,7 @@ import com.plcoding.doodlekong.adapters.PlayerAdapter
 import com.plcoding.doodlekong.data.remote.ws.Room
 import com.plcoding.doodlekong.data.remote.ws.models.*
 import com.plcoding.doodlekong.databinding.ActivityDrawingBinding
+import com.plcoding.doodlekong.ui.dialogs.LeaveDialog
 import com.plcoding.doodlekong.util.Constants
 import com.plcoding.doodlekong.util.hideKeyboard
 import com.tinder.scarlet.WebSocket
@@ -417,5 +418,14 @@ class DrawingActivity : AppCompatActivity(), LifecycleObserver {
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     private fun onAppInBackground() {
         viewModel.disconnect()
+    }
+
+    override fun onBackPressed() {
+        LeaveDialog().apply {
+            setPositiveClickListener {
+                viewModel.disconnect()
+                finish()
+            }
+        }.show(supportFragmentManager, null)
     }
 }
